ba0cd3c852d22066675781701f5a1b0c0f88dc9e
Dishan Kheni <dishankkheni.dev@gmail.com>
2023-10-25 17:35:13 +0530

    feat : bucket sort

---
+++ 2 Sorting/bucket_sort.py
@@ -0,0 +1,18 @@
+def bucketSort():
+
+    arr = [7, 500, 26, 6, 668, 578, 45, 28, 3974, 175]
+
+    bucket = [[] for _ in range(len(str(max(arr))))]
+
+    for i in range(len(arr)):
+        size = len(str(arr[i]))
+        bucket[size-1].append(arr[i])
+
+    for i in range(len(bucket)):
+        bucket[i] = sorted(bucket[i])
+
+    for each in bucket:
+        print(each)
+
+
+bucketSort()
